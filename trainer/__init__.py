e2bba00d759e82fc0d8928ff601939137b6b5ebe
JiiahaoXU <535846269@qq.com>
2024-09-10 15:03:26 -0700

    all

--- trainer/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .CycTrainer import Cyc_Trainer
-from .NiceTrainer import Nice_Trainer
-from .p2pTrainer import P2p_Trainer
-from .UnitTrainer import Unit_Trainer
-from .MunitTrainer import Munit_Trainer
-from .ReconfigMIT_Trainer import Reconfig_MIT_Trainer
